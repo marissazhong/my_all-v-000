9fcdd6453fa3816a2cd2cb21fff397db9af1fe89
Marissa Zhong <mwz2101@columbia.edu>
2018-06-26 11:44:11 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -7,6 +7,13 @@ def my_all?(collection)
     block_return_values << yield(collection[i])
     i+=1
   end
+
+  if block_return_values.include?(false)
+    false
+  else
+    true
+  end
+  
 end
 
 my_arr?([1,2,3]) {|i| i < 2}
